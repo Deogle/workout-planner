a450652d6621fabd8cb6d2d10e4a2a41ffd58772
Deogle <deogle20@gmail.com>
2020-02-11 12:12:39 -0500

    refactored tests to work with new musicFiles model

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -7,10 +7,10 @@ export const REMOVE_AUDIO = 'REMOVE_AUDIO'
 /**
  * action creators
  */
-export const addAudio =  filename => {
-    return { type: ADD_AUDIO, filename:filename}
+export const addAudio =  file => {
+    return { type: ADD_AUDIO, filename:file.filename,resource_url:file.resource_url}
 }
 
-export const removeAudio = filename => {
-    return { type: REMOVE_AUDIO, filename:filename}
+export const removeAudio = file => {
+    return { type: REMOVE_AUDIO, filename:file.filename,resource_url:file.resource_url}
 }
--- src/tests/Actions.test.js
+++ src/tests/Actions.test.js
@@ -2,21 +2,24 @@
 import * as actions from '../redux/actions';
 
 describe('actions', () => {
+    const file = {filename:'test.mp3',resource_url:'https://example.url/test.mp3'};
+
   it('should create an action to add an audio file',()=>{
-    const filename = 'test.mp3'
     const expectedAction = {
       type: actions.ADD_AUDIO,
-      filename
+      filename:file.filename,
+      resource_url:file.resource_url
     }
-    expect(actions.addAudio(filename)).toEqual(expectedAction);
+    expect(actions.addAudio(file)).toEqual(expectedAction);
   })
 
   it('should create an action to remove an audio file',()=>{
-    const filename = 'test.mp3'
+    const file = {filename:'test.mp3',resource_url:'https://example.url/test.mp3'}
     const expectedAction = {
       type:actions.REMOVE_AUDIO,
-      filename
+      filename:file.filename,
+      resource_url:file.resource_url
     }
-    expect(actions.removeAudio(filename)).toEqual(expectedAction);
+    expect(actions.removeAudio(file)).toEqual(expectedAction);
   })
 })
